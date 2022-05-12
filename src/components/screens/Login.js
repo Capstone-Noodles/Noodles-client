03ba5854ffc8e046fc1d678ddf3e8aef5c3d31f0
Seungyong Choi <ppyooy336@naver.com>
2022-05-12 14:56:07 +0900

    Add import axios

--- src/components/screens/Login.js
+++ src/components/screens/Login.js
@@ -1,46 +1,54 @@
-import { NavigationContainer } from '@react-navigation/native';
-import React, { useRef, useContext } from 'react';
-import { Text, View, Keyboard, Image, StatusBar, TouchableOpacity, Alert } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import React, { useRef, useContext } from "react";
+import {
+  Text,
+  View,
+  Keyboard,
+  Image,
+  StatusBar,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 //import { AsyncStorage } from '@react-native-async-storage/async-storage';
-import Input from '../screenComponents/Input';
-import Loader from '../screenComponents/Loader';
+import Input from "../screenComponents/Input";
+import Loader from "../screenComponents/Loader";
 import { UserContext, UserProvider } from "../../contexts/User";
+import axios from "axios";
 
-const Login = ({navigation})=> {
-
-  const [inputs,setInputs] = React.useState({ 
-    id:'',
-    password:'',
+const Login = ({ navigation }) => {
+  const [inputs, setInputs] = React.useState({
+    id: "",
+    password: "",
   });
-  const [errors,setErrors] = React.useState({});
-  const [loading,setLoading] = React.useState(false);
+  const [errors, setErrors] = React.useState({});
+  const [loading, setLoading] = React.useState(false);
 
   const validate = () => {
-      Keyboard.dismiss();
-      inputs.id = inputs.id.replace(/ /g,"").trim();
-      inputs.password = inputs.password.replace(/ /g,"").trim();
-      
-      let valid = true;
-      if (!inputs.id) {
-        handleError('아이디를 입력해주세요!','id');
-        valid=false;
-      }
-      if (!inputs.password) {
-        handleError('비밀번호을 입력해주세요!','password');
-        valid=false;
-      } else if (inputs.password.length < 5) {
-        handleError('5자 이상의 비밀번호을 입력해주세요!','password');
-        valid=false;
-      }
-      if(valid) {
-          //console.log([inputs.id,inputs.password])
-          login();
-      }
+    Keyboard.dismiss();
+    inputs.id = inputs.id.replace(/ /g, "").trim();
+    inputs.password = inputs.password.replace(/ /g, "").trim();
+
+    let valid = true;
+    if (!inputs.id) {
+      handleError("아이디를 입력해주세요!", "id");
+      valid = false;
+    }
+    if (!inputs.password) {
+      handleError("비밀번호을 입력해주세요!", "password");
+      valid = false;
+    } else if (inputs.password.length < 5) {
+      handleError("5자 이상의 비밀번호을 입력해주세요!", "password");
+      valid = false;
+    }
+    if (valid) {
+      //console.log([inputs.id,inputs.password])
+      login();
+    }
   };
 
   const { dispatch } = useContext(UserContext);
 
-    // 로그인 axios
+  // 로그인 axios
   const pressLoginBtn = async () => {
     await axios
       .post("http://133.186.228.218:8080/users/login", {
@@ -78,9 +86,9 @@ const Login = ({navigation})=> {
       try {
         //AsyncStorage.setItem("user",JSOM.stringify(inputs));
         navigation.navigate("Bottom");
-    } catch (error) {
-        Alert.alert("Error",'존재하지 않는 회원입니다!');
-    }
+      } catch (error) {
+        Alert.alert("Error", "존재하지 않는 회원입니다!");
+      }
       /*let userData = await AsyncStorage.getItem('user');
       if (userData) {
         userData = JSON.parse(userData);
@@ -98,53 +106,71 @@ const Login = ({navigation})=> {
     }, 2000);
   };
   const handleOnChange = (text, input) => {
-      setInputs((prevState)=>({...prevState,[input]:text}));
+    setInputs((prevState) => ({ ...prevState, [input]: text }));
   };
-  const handleError = (errorMessage,input)=>{
-      setErrors((prevState)=>({...prevState,[input]:errorMessage}));
+  const handleError = (errorMessage, input) => {
+    setErrors((prevState) => ({ ...prevState, [input]: errorMessage }));
   };
-
 
   return (
-    <View style={{flex:1,backgroundColor:'white'}}>
-      <Loader visible={loading}/>
-      <StatusBar backgroundColor='white' barStyle="dark-content" animated={true}/>
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <Loader visible={loading} />
+      <StatusBar
+        backgroundColor="white"
+        barStyle="dark-content"
+        animated={true}
+      />
 
-      <View style={{
-        justifyContent:'center',
-        alignItems:'center',
-        paddingVertical:50,
-       }}>
-        <Image source={require('../../storage/images/logo.jpg')}/>
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          paddingVertical: 50,
+        }}
+      >
+        <Image source={require("../../storage/images/logo.jpg")} />
       </View>
 
+      <View style={{ padding: 20 }}>
+        <Input
+          label="ID"
+          placeholder="영문ID"
+          iconName="person-outline"
+          error={errors.id}
+          onFocus={() => {
+            handleError(null, "id");
+          }}
+          onChangeText={(text) => handleOnChange(text, "id")}
+        />
+        <Input
+          label="Password"
+          placeholder="5자 이상"
+          iconName="md-lock-closed-outline"
+          error={errors.password}
+          onFocus={() => {
+            handleError(null, "password");
+          }}
+          onChangeText={(text) => handleOnChange(text, "password")}
+          password
+        />
 
-      <View style={{padding:20}}>
-          <Input 
-            label="ID" 
-            placeholder="영문ID"
-            iconName="person-outline"
-            error={errors.id}
-            onFocus={()=>{ handleError(null,'id'); }}
-            onChangeText={(text)=>handleOnChange(text,'id')} />
-          <Input 
-            label="Password" 
-            placeholder="5자 이상"
-            iconName="md-lock-closed-outline"
-            error={errors.password}
-            onFocus={()=>{ handleError(null,'password'); }}
-            onChangeText={(text)=>handleOnChange(text,'password')}
-            password />
-          
-          <TouchableOpacity onPress={pressLoginBtn} activeOpacity={0.7}>
-            <View style={{
-                backgroundColor:'#ffbfbf', borderRadius:10,
-                width:'100%', height:45, marginTop:20,
-                alignItems:'center', justifyContent:'center', 
-            }}>
-                <Text style={{color:'#fff',fontSize:20,fontWeight:'bold'}}>로그인</Text>
-            </View>
-          </TouchableOpacity>    
+        <TouchableOpacity onPress={pressLoginBtn} activeOpacity={0.7}>
+          <View
+            style={{
+              backgroundColor: "#ffbfbf",
+              borderRadius: 10,
+              width: "100%",
+              height: 45,
+              marginTop: 20,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Text style={{ color: "#fff", fontSize: 20, fontWeight: "bold" }}>
+              로그인
+            </Text>
+          </View>
+        </TouchableOpacity>
       </View>
       {/*<View style={{marginTop:20}}>
       <Text style={{fontWeight:'bold', color:'gray'}}>  ID</Text>
@@ -175,34 +201,27 @@ const Login = ({navigation})=> {
       </View>
       </TouchableOpacity>*/}
 
-      <View style={{
-        flexDirection:'row', 
-        paddingVertical:10, 
-        alignItems:'center', 
-        justifyContent:'center'}}>       
-      <TouchableOpacity>
-        <Text style={{paddingRight:10,fontSize:13}}>
-          아이디 찾기   
-        </Text>
-      </TouchableOpacity>
-      <Text style={{color:'gray',fontSize:13}}>|   </Text>
-      <TouchableOpacity>
-        <Text style={{paddingRight:10,fontSize:13}}>
-          비밀번호 찾기
-        </Text>
-      </TouchableOpacity>
-      <Text style={{color:'gray',fontSize:13}}>|   </Text>
-      <TouchableOpacity onPress={()=>navigation.navigate("Signup")}>
-        <Text style={{fontSize:13}}>
-          회원가입
-        </Text>
-      </TouchableOpacity>
+      <View
+        style={{
+          flexDirection: "row",
+          paddingVertical: 10,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <TouchableOpacity>
+          <Text style={{ paddingRight: 10, fontSize: 13 }}>아이디 찾기</Text>
+        </TouchableOpacity>
+        <Text style={{ color: "gray", fontSize: 13 }}>| </Text>
+        <TouchableOpacity>
+          <Text style={{ paddingRight: 10, fontSize: 13 }}>비밀번호 찾기</Text>
+        </TouchableOpacity>
+        <Text style={{ color: "gray", fontSize: 13 }}>| </Text>
+        <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
+          <Text style={{ fontSize: 13 }}>회원가입</Text>
+        </TouchableOpacity>
       </View>
-
-      
-
     </View>
-    
   );
 };
 
