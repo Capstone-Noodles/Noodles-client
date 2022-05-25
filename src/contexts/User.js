ff8ce1422c00c35a5840d618ce52c2257a81c592
wormjoo <wormjoo@naver.com>
2022-05-26 02:05:12 +0900

    Add post by location

--- src/contexts/User.js
+++ src/contexts/User.js
@@ -1,14 +1,14 @@
 import React, { useState, createContext } from 'react';
 
 const UserContext = createContext({
-    user: { accessToken: null, refreshToken: null },
+    user: { accessToken: null, refreshToken: null, id: null, location: null, latitude: null, longitude: null },
     dispatch: () => {},
 });
 
 const UserProvider = ({ children }) => {
     const [user, setUser] = useState({});
-    const dispatch = ({ accessToken, refreshToken }) => {
-        setUser({ accessToken, refreshToken });
+    const dispatch = ({ accessToken, refreshToken, id, location, latitude, longitude }) => {
+        setUser({ accessToken, refreshToken, id, location, latitude, longitude });
     };
     const value = { user, dispatch };
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
