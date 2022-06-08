57c60524f5a5e1ef28190332c3b71e2f8d156614
wormjoo <wormjoo@naver.com>
2022-06-08 23:26:27 +0900

    Fix peoplepost

--- src/components/screenComponents/PeoplePost.js
+++ src/components/screenComponents/PeoplePost.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useCallback } from "react";
 import {
   Text,
   View,
@@ -42,7 +42,7 @@ const Item = React.memo(
       identification,
     },
   }) => {
-    const [like, setLike] = useState(likes);
+    const [like, setLike] = useState(isLiked);
     const [bookmark, setBookmark] = useState(isBookmarked);
     const [modalVisible, setModalVisible] = useState(false);
     const [others_modalVisible, setOthers_ModalVisible] = useState(false);
@@ -50,6 +50,7 @@ const Item = React.memo(
     const { user } = useContext(UserContext);
     const userId = user.id;
     const [viewMore, setViewMore] = useState(false);
+    const { dispatch } = useContext(UserContext);
 
     const navigation = useNavigation();
 
@@ -62,6 +63,71 @@ const Item = React.memo(
         setOthers_ModalVisible(!others_modalVisible);
       }
     };
+
+    const _handleLikePress = useCallback(async () => {
+      try {
+        axios({
+          method: "post",
+          url: "http://133.186.228.218:8080/posts/like/" + postIdx,
+          headers: {
+            "x-auth-token": `${user?.accessToken}`,
+          },
+        })
+          .then(function (response) {
+            if (like == 0) {
+              setLike(1);
+            } else {
+              setLike(0);
+            }
+            dispatch({
+              accessToken: user.accessToken,
+              refreshToken: user.refreshToken,
+              id: user.id,
+              location: user.location,
+              latitude: user.latitude,
+              longitude: user.longitude,
+            });
+            return response.data;
+          })
+          .catch(function (error) {
+            console.log(error);
+            alert("Error", error);
+          });
+      } catch (e) {
+      } finally {
+      }
+    }, [user, postIdx, setLike, dispatch]);
+
+    const _handleDeletePress = useCallback(async () => {
+      try {
+        axios({
+          method: "patch",
+          url: "http://133.186.228.218:8080/posts/delete/" + postIdx,
+          headers: {
+            "x-auth-token": `${user?.accessToken}`,
+          },
+        })
+          .then(function (response) {
+            dispatch({
+              accessToken: user.accessToken,
+              refreshToken: user.refreshToken,
+              id: user.id,
+              location: user.location,
+              latitude: user.latitude,
+              longitude: user.longitude,
+            });
+            setModalVisible(!modalVisible);
+            Alert.alert("게시글이 삭제되었습니다.");
+            return;
+          })
+          .catch(function (error) {
+            console.log(error);
+            alert("Error", error);
+          });
+      } catch (e) {
+      } finally {
+      }
+    }, [user, postIdx, dispatch, setModalVisible, modalVisible]);
 
     return (
       <View>
@@ -107,23 +173,24 @@ const Item = React.memo(
                   style={{ fontSize: 20, paddingRight: 10 }}
                 />
               </TouchableOpacity>
-              <TouchableOpacity onPress={() => navigation.navigate("Comment", postIdx)}>
+              <TouchableOpacity
+                onPress={() => navigation.navigate("Comment", postIdx)}
+              >
                 <Ionic
                   name="ios-chatbubble-outline"
                   style={{ fontSize: 20, paddingRight: 10 }}
                 />
               </TouchableOpacity>
-              <TouchableOpacity onPress={() => setLike(!like)}>
-                <AntDesign
-                  name={like ? "heart" : "hearto"}
-                  style={{
-                    paddingRight: 10,
-                    fontSize: 20,
-                    color: like ? "tomato" : "black",
-                  }}
-                />
-              </TouchableOpacity>
-              <Text>좋아요 {like ? likes + 1 : likes}개</Text>
+              <AntDesign
+                name={like ? "heart" : "hearto"}
+                style={{
+                  paddingRight: 10,
+                  fontSize: 20,
+                  color: like ? "tomato" : "black",
+                }}
+                onPress={_handleLikePress}
+              />
+              <Text>좋아요 {likes}개</Text>
             </View>
             <TouchableOpacity style={{ paddingLeft: 10 }} onPress={popup}>
               <Feather name="more-vertical" style={{ fontSize: 20 }} />
